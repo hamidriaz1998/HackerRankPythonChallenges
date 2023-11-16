d8dddee7ac8f49f4d59ec3c1351da37d9cdb7cc7
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-11-16 20:11:48 +0500

    Complete Challenge

---
+++ 12.Lists.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    N = int(input())
+    commands=[]
+    for i in range(N):
+        command = input()
+        commands.append(command)
+    commands = [i.split() for i in commands]
+    commands = [[int(j) if j.isdigit() else j for j in i] for i in commands]
+    mainList= []
+    for i in commands:
+        if i[0] == "insert":
+            mainList.insert(i[1],i[2])
+        elif i[0] == "print":
+            print(mainList)
+        elif i[0] == "remove":
+            mainList.remove(i[1])
+        elif i[0] == "append":
+            mainList.append(i[1])
+        elif i[0] == "sort":
+            mainList.sort()
+        elif i[0] == "pop":
+            mainList.pop()
+        elif i[0] == "reverse":
+            mainList.reverse()
