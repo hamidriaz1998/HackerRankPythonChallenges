a01b92da35ead0e01c68f03fa21e1265eef146bb
Hamid Riaz <111028733+hamidriaz1998@users.noreply.github.com>
2024-01-02 15:26:18 +0000

    Fix code

--- CodeFiles/37.mapAndLambda.py
+++ CodeFiles/37.mapAndLambda.py
@@ -3,7 +3,8 @@
 cube = lambda x: x**3
 
 def fibonacci(n):
-    fibs = [0, 1]
+    fibs = []
+    fibs.append(0) if n == 1 else fibs.extend([0, 1]) if n > 1 else fibs
     while len(fibs) < n:
         fibs.append(fibs[-1] + fibs[-2])
     return fibs
